a52d0f05ff29abf5cc7ee94cecc38db9ba9e1949
Ivana Drábiková <idrabikova@gmail.com>
2015-08-26 11:47:16 +0200

    Insert Function

--- AVLTree.py
+++ AVLTree.py
@@ -1,9 +1,10 @@
 __author__ = 'Carmen'
+import Node
 
 class AVLTree:
 
     def __init__(self):
-        self.Node = None
+        self.node = None
         self.height = -1
         self.balance = 0
 
@@ -11,7 +12,23 @@ class AVLTree:
         return (self.height == 0)
 
     def insert(self, key):
+        tree = self.node
+        newnode = Node(key)
 
+        if not self.node:
+            self.node = newnode
+            self.node.left = AVLTree()
+            self.node.right = AVLTree()
+
+        elif key < tree.key:
+            self.node.left.insert(key)
+
+        elif key > tree.key:
+            self.node.right.insert(key)
+
+        # else: key is already in the 3
+
+        self.rebalance()
 
     def rebalance(self):
 
