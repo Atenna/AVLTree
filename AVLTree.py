c4a1882538c0c9173a6a62f5d77ea12aa7b20b66
Ivana Drábiková <idrabikova@gmail.com>
2015-08-26 11:18:34 +0200

    Initial Commit

---
+++ AVLTree.py
@@ -0,0 +1,17 @@
+__author__ = 'Carmen'
+
+class AVLTree:
+
+    def __init__(self):
+        self.Node = None
+        self.height = -1
+        self.balance = 0
+
+    def is_leaf(self):
+        return (self.height == 0)
+
+    def insert(self, key):
+
+
+    def rebalance(self):
+
